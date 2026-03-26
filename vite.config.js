e807e6deb39440a69d1af8c8a3641f6ef17f4beb
Carlos García Huerta <carlosghta9999@gmail.com>
2026-03-25 20:24:09 -0600

    Primer commit del dino-app

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- IMPORTANTE
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <--- DEBE ESTAR AQUÍ
-  ],
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Subimos el límite a 1000kb para que no nos moleste
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Separamos las librerías pesadas (vendor) del código de nuestra lógica
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
